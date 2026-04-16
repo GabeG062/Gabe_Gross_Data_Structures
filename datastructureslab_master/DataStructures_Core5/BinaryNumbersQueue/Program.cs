a96f352506e56f5f9c276dc00eb3b85b61fb1dcf
starman062 <gabrielgross222@gmail.com>
2026-04-15 22:51:59 -0700

    Finished String Reverse

--- datastructureslab_master/DataStructures_Core5/BinaryNumbersQueue/Program.cs
+++ datastructureslab_master/DataStructures_Core5/BinaryNumbersQueue/Program.cs
@@ -6,8 +6,7 @@ namespace BinaryNumbersQueue
     {
         static void Main(string[] args)
         {
-
-        }
             
+        }
     }
 }
--- datastructureslab_master/DataStructures_Core5/StringReverseStack/Program.cs
+++ datastructureslab_master/DataStructures_Core5/StringReverseStack/Program.cs
@@ -5,9 +5,36 @@ namespace StringReverseStack
 {
     class Program
     {
+        //enter a string from the keyboard and prints the string in reverse order, EX: typed in "Hello", get back "olleH"
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a sentence or numbers");
+            string input = Console.ReadLine();
+            char[] combinedInput = input.ToCharArray();
+            string[] combinedString = new string[combinedInput.Length];
+            for (int i = 0; i < combinedInput.Length; i++)
+            {
+                combinedString[i] = combinedInput[i].ToString();
+            }
+            Console.WriteLine($"combinedString length: {combinedString.Length}");
+            Console.WriteLine();
+            Console.WriteLine($"the reversed version of {input} is {Reverser(combinedString)}");
+        }
+
+        static string Reverser(string[] combinedString)
+        {
             
+            Stack<string> stack = new Stack<string>(combinedString);
+            int length = stack.Count;
+            string[] reversedArray = new string[stack.Count];
+            string reversedResult = string.Empty;
+            
+            for (int i = 0; i < length; i++ )
+            {
+                reversedArray[i] = stack.Pop();
+            }
+            reversedResult = string.Join("", reversedArray);
+            return reversedResult;
         }
     }
 }
